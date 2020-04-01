184e90c2a1e9906d648faa5df14c5134adcf5298
Joseph Kao <josephtkao@gmail.com>
2020-04-01 22:04:36 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,9 @@
 def turn_count(board)
   count = 0
-  board.each do
-    count += 1
-  end
+  board.each |pos| do
+    if (pos == "" || pos == " " || pos == nil
+    else
+      count += 1
+    end
 end
   
